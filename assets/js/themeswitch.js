23e811152ad332ad3f7c30f4ddbcc9dc6ed6beec
Vincent Sylvester <vincentsylvester@zetamex.com>
2018-02-18 20:25:46 +0100

    Some more prep work

--- assets/js/themeswitch.js
+++ assets/js/themeswitch.js
@@ -5,29 +5,41 @@ var z = document.getElementsByClassName("social-contact-icon");
 var u = document.getElementsByClassName("icon");
 var j = document.getElementsByClassName("inner");
 var k = document.getElementsByTagName("hr");
+var l = document.getElementById("button");
 
-var color;
+var color1;
+var color2;
+var color3;
+var color4;
+var color5;
 
 if (t.childNodes[0].innerHTML == "Dark Theme")
 {
     t.childNodes[0].innerHTML = "Bright Theme";
-    document.body.style.backgroundColor = "gray";
-	document.getElementById("button").style.backgroundColor = "#f5f5f5";
-	color = "gray";
+	color1 = "#333";
 	color2 = "black";
-	color3 = "#BDBDBD";
+	color3 = "#222";
+	color4 = "#d2d2d2";
+	color5 = "#f5f5f5";
+    document.body.style.backgroundColor = color1;
+	document.body.style.color = color4;
+	document.getElementById("button").style.backgroundColor = color5;
 }
 else
 {
     t.childNodes[0].innerHTML = "Dark Theme";
-    document.body.style.backgroundColor = "white";
-	document.getElementById("button").style.backgroundColor = "gray";
-	color = "#f5f5f5";
+	color1 = "#f5f5f5";
 	color2 = "#e3e3e3"
 	color3 = "#f5f5f5";
+	color4 = "#333";
+	color5 = "#333";
+    document.body.style.backgroundColor = color1;
+	document.getElementById("button").style.backgroundColor = color5;
+	document.body.style.color = color4;
 }
 
 var i;
+l.style.color = color1;
 for (i = 0; i < x.length; i++)
 {
 	x[i].style.backgroundColor = color3;
@@ -35,15 +47,15 @@ for (i = 0; i < x.length; i++)
 }
 for (i = 0; i < y.length; i++)
 {
-	y[i].style.backgroundColor = color;
+	y[i].style.backgroundColor = color1;
 }
 for (i = 0; i < z.length; i++)
 {
-	z[i].style.backgroundColor = color;
+	z[i].style.backgroundColor = color1;
 }
 for (i = 0; i < u.length; i++)
 {
-	u[i].style.borderColor = color;
+	u[i].style.borderColor = color1;
 }
 for (i = 0; i < j.length; i++)
 {
